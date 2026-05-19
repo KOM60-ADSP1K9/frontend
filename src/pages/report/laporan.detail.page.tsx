6afba82586bdb094cadeeb23525d8cde1f02e3ab
mahesgya <anargya115@gmail.com>
2026-05-19 22:08:06 +0700

    fix: error operator

--- src/pages/report/laporan.detail.page.tsx
+++ src/pages/report/laporan.detail.page.tsx
@@ -172,7 +172,7 @@ class LaporanDetailPageBase extends React.Component<RouterProps, State> {
     const { barang, type, status, user, is_owned } = laporan;
     const isFound = type === 'temuan';
     const locationEmbedded = isFound ? laporan.found_at_location : laporan.lost_at_location;
-    const locationName = locationEmbedded?.name ?? (locationEmbedded?.id && lokasiMap[locationEmbedded.id]) || '—';
+    const locationName = locationEmbedded?.name ?? ((locationEmbedded?.id && lokasiMap[locationEmbedded.id]) || '—');
     const eventDate = isFound ? laporan.found_at_date : laporan.lost_at_date;
     const canUpdateStatus = is_owned && LaporanService.canUpdate(status);
     const canEditLaporan = is_owned && LaporanService.canEdit(status);
